95e0667381b8a4a29fd33deb84349e62f1887759
Drake <drakem@gmail.com>
2022-02-14 14:38:44 -0500

    added basis for collecting tracker data

--- nodes/training_data_recorder.py
+++ nodes/training_data_recorder.py
@@ -3,7 +3,7 @@ import rospy
 import rospkg
 import numpy as np
 import sys
-from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Pose, PoseStamped
 from rospy.rostime import Duration
 from sensor_msgs.msg import JointState
 from multiprocessing import Lock
@@ -24,17 +24,23 @@ class DataRecorder(object):
         # Init the topics
         self.joint_state_sub = rospy.Subscriber("/" + self.robot_name + "/base_feedback/joint_state", JointState, 
             self.joint_state_cb)
+        self.tracker_sub = rospy.Subscriber("/wrist_pose", PoseStamped, self.tracker_cb)
         
 
         # Global data
         self.robot_demo_traj = []
         self.enable_record = False
-        self.sample_rate = 100 #Hz
-        self.total_samples = 0
+        self.robot_sample_rate = 100 #Hz
+        self.total_robot_samples = 0
         self.data_addr = "/home/rui/kinova_ws/src/kortex_playground/data/"
     
+        self.tracker_demo_traj = []
+        self.tracker_sample_rate = 10 # Unknown right now. Will update as needed.
+        self.total_tracker_samples = 0
+
+
     def joint_state_cb(self, msg):
-        if self.total_samples != 0 and len(self.robot_demo_traj) < self.total_samples:
+        if self.total_robot_samples != 0 and len(self.robot_demo_traj) < self.total_robot_samples:
             # The joint angle range of JointState is -pi~pi, we need to convert it into 0~2pi
             if msg.position < 0:
                 pos = msg.position + PI
@@ -47,14 +53,27 @@ class DataRecorder(object):
             entry = pos[:-1] + vel[:-1] + eff[:-1]
             assert(len(entry) == 7*3), "Data entry length is not correct."
             self.robot_demo_traj.append(entry)
+
+    def tracker_cb(self, msg):
+        if self.total_tracker_samples != 0 and len(self.tracker_demo_traj) < self.total_tracker_samples:
+            tracker_pose = msg.pose
+            # Only care about the position of the wrist tracker
+            self.tracker_demo_traj.append([tracker_pose.position.x, tracker_pose.position.y, tracker_pose.position.z])
+
     
     def start_record(self, duration):
         # record data in duration secs
-        self.total_samples = duration * self.sample_rate
-    
+        self.total_robot_samples = duration * self.robot_sample_rate
+        self.total_tracker_samples = duration * self.tracker_sample_rate
+
     def save_data(self):
-        data_frame = np.array(self.robot_demo_traj)
-        np.savetxt(self.data_addr+'test.csv', data_frame , delimiter=",")
+        robot_data_frame = np.array(self.robot_demo_traj)
+        tracker_data_frame = np.array(self.tracker_demo_traj)
+
+        # Will need to look into what to do if the data is gathered at different rates and the matrix sizes do not align
+        data_frame = np.append(robot_data_frame, tracker_data_frame, axis=1)
+
+        np.savetxt(self.data_addr+'test.csv', robot_data_frame , delimiter=",")
         rospy.loginfo("Training data saved to "+self.data_addr+'test.csv')
 
 
